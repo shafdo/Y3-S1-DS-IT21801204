a0f35605d047f3f8cd5b557c008e867a1ef2f122
Binoosh <wbinoosh@gmail.com>
2024-05-07 00:51:56 +0530

    Validations added to all functions implemented so far

--- course-management-service/controllers/courseController.js
+++ course-management-service/controllers/courseController.js
@@ -3,34 +3,40 @@ const Course = require('../models/Course');
 const { v4: uuidv4 } = require('uuid');
 
 async function addCourse(req, res) {
-    let crscode = generateUniqueCourseCode(); // Generate a random crscode
-    const instructorId = 'in2d3s5ef534';
-    const crsname = req.body.crsname;
-    const description = req.body.description;
-    const price = Number(req.body.price);
-    try {
-        // Check if a course with the same crscode alrady exists
-        let existingCourse = await Course.findOne({ crscode });
-        while (existingCourse) {
-            // If course with the same crscode already exists, regenerate a new crscode
-            crscode = generateUniqueCourseCode();
-            existingCourse = await Course.findOne({ crscode });
+    const role = 'instructor';
+    if(role === 'instructor'){
+        let crscode = generateUniqueCourseCode(); // Generate a random crscode
+        const instructorId = 'in2d3s5ef534';
+        const crsname = req.body.crsname;
+        const description = req.body.description;
+        const price = Number(req.body.price);
+        try {
+            // Check if a course with the same crscode alrady exists
+            let existingCourse = await Course.findOne({ crscode });
+            while (existingCourse) {
+                // If course with the same crscode already exists, regenerate a new crscode
+                crscode = generateUniqueCourseCode();
+                existingCourse = await Course.findOne({ crscode });
+            }
+    
+            // Create and save the new course
+            const courseItem = new Course({
+                instructorId,
+                crscode,
+                crsname,
+                description,
+                price
+            });
+    
+            await courseItem.save();
+            return res.json({ status: "Course successfully added!" });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ status: "Error with adding course" });
         }
-
-        // Create and save the new course
-        const courseItem = new Course({
-            instructorId,
-            crscode,
-            crsname,
-            description,
-            price
-        });
-
-        await courseItem.save();
-        return res.json({ status: "Course successfully added!" });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ status: "Error with adding course" });
+    }
+    else{
+        return res.status(401).json({ error: "You are not authorized to perform this action" });
     }
 }
 
@@ -40,7 +46,7 @@ function generateUniqueCourseCode() {
     return uuidv4();
 }
 
-// An admin can get all the courses, but for students and instructors they can only fetch courses with the status approved.
+// An admin can get all the courses, but for students and instructors they can only fetch courses with the status approved. This simply gets a list of all available courses.
 async function getAllCourses(req, res){
     const role = 'learner' // this is a sample value (remove later)
     if(role === 'admin'){
@@ -64,8 +70,9 @@ async function getAllCourses(req, res){
 
 }
 
+// Using the below function users will be able to get data regarding a single course.
 async function getCourseById(req, res){
-    const crscode = req.params.crscode; // Assuming the course code is passed as a URL parameter
+    const crscode = req.params.crscode;
     try {
         const courseItem = await Course.findOne({ crscode });
         if (!courseItem) {
@@ -79,77 +86,91 @@ async function getCourseById(req, res){
 }
 
 async function updateCourse(req, res) {
-    const { crscode } = req.params;
-    const { crsname, description, price } = req.body;
-    const courseDetails = await Course.findOne({crscode});
-    let instructorId;
-    let currentUserId = 'in2d3s5ef534'
-    // let currentUserId = 'in2d3s5ef53'
-    if(courseDetails){
-        instructorId = courseDetails.instructorId;
-    }
-    else{
-        return res.status(404).json({ error: "Course with provided code not found" });
-    }
-
-    if(currentUserId === instructorId){
-        try {
-            // Find the course by crscode and update it
-            const updatedItem = await Course.findOneAndUpdate(
-                { crscode },
-                { $set: { crsname, description, price } },
-                { new: true } // Return the updated document
-            );
+    const role = 'instructor';
+    if(role === 'instructor'){
+        const { crscode } = req.params;
+        const { crsname, description, price } = req.body;
+        const courseDetails = await Course.findOne({crscode});
+        let instructorId;
+        let currentUserId = 'in2d3s5ef534'
+        // let currentUserId = 'in2d3s5ef53'
+        if(courseDetails){
+            instructorId = courseDetails.instructorId;
+        }
+        else{
+            return res.status(404).json({ error: "Course with provided code not found" });
+        }
     
-            if (updatedItem) {
-                return res.status(200).json({ status: "Item updated", updatedItem });
-            } else {
-                return res.status(404).json({ error: "Course with provided code not found" });
+        if(currentUserId === instructorId){
+            try {
+                // Find the course by crscode and update it
+                const updatedItem = await Course.findOneAndUpdate(
+                    { crscode },
+                    { $set: { crsname, description, price } },
+                    { new: true } // Return the updated document
+                );
+        
+                if (updatedItem) {
+                    return res.status(200).json({ status: "Item updated", updatedItem });
+                } else {
+                    return res.status(404).json({ error: "Course with provided code not found" });
+                }
+            } catch (err) {
+                console.log(err);
+                return res.status(500).json({ error: "Internal server error" });
             }
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({ error: "Internal server error" });
+        }
+        else{
+            return res.status(401).json({ error: "You are not authorized to perform this action" });
         }
     }
     else{
         return res.status(401).json({ error: "You are not authorized to perform this action" });
     }
+    
 
     
 }
 
 
 async function deleteCourse(req, res){
-    const { crscode } = req.params;
-    const courseDetails = await Course.findOne({crscode});
-    let instructorId;
-    let currentUserId = 'in2d3s5ef534'
-    // let currentUserId = 'in2d3s5ef53'
-    if(courseDetails){
-        instructorId = courseDetails.instructorId;
-        console.log(instructorId);
-    }
-    else{
-        return res.status(404).json({ error: "Course with provided code not found" });
-    }
-    if(instructorId === currentUserId){
-        try {
-            // Find the course by crscode and delete it
-            const deletedItem = await Course.findOneAndDelete({ crscode });
-    
-            if (deletedItem) {
-                return res.status(200).json({ status: "Item deleted", deletedItem });
-            } else {
-                return res.status(404).json({ error: "Course with the entered code not found" });
+    const role = 'instructor';
+    if(role === 'instructor'){
+        const { crscode } = req.params;
+        const courseDetails = await Course.findOne({crscode});
+        let instructorId;
+        let currentUserId = 'in2d3s5ef534'
+        // let currentUserId = 'in2d3s5ef53'
+        if(courseDetails){
+            instructorId = courseDetails.instructorId;
+            console.log(instructorId);
+        }
+        else{
+            return res.status(404).json({ error: "Course with provided code not found" });
+        }
+        if(instructorId === currentUserId){
+            try {
+                // Find the course by crscode and delete it
+                const deletedItem = await Course.findOneAndDelete({ crscode });
+        
+                if (deletedItem) {
+                    return res.status(200).json({ status: "Item deleted", deletedItem });
+                } else {
+                    return res.status(404).json({ error: "Course with the entered code not found" });
+                }
+            } catch (err) {
+                console.log(err);
+                return res.status(500).json({ error: "Internal server error" });
             }
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({ error: "Internal server error" });
+        }
+        else{
+            return res.status(401).json({ error: "You are not authorized to perform this action" });
         }
     }
     else{
         return res.status(401).json({ error: "You are not authorized to perform this action" });
     }
+    
 
     
 }
