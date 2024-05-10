1151baa2ccc388baa15038dab85b4e9ab8d01c43
RadithSandeepa <radithsandeepa500@gmail.com>
2024-05-10 19:09:06 +0530

    Implemented learner management service

---
+++ enrollment-management-system/routes/enrollments.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { addEnrollment, deleteCourseEnrollment, deleteCourseInEnrollments, deleteEnrollment, getEnrollmentStatus } from '../controllers/enrollments.js';
+
+const router = express.Router();
+
+//CREATE ENROLLMENT
+router.post("/:courseId/:userId/add", addEnrollment);
+
+//CHECK ENROLLMENT STATUS
+router.get("/:courseId/:userId", getEnrollmentStatus);
+
+//DELETE ENROLLMENT TO A SINGLE COURSE OF SINGLE USER ENROLLMENTS
+router.delete("/:courseId/:userId", deleteCourseEnrollment);
+
+//DELETE ALL ENROLLMENT TO A SINGLE USER
+router.delete("/:userId", deleteEnrollment);
+
+//DELETE ALL ENROLLMENTS TO A COURSE
+router.delete("/remove/course/:courseId", deleteCourseInEnrollments);
+
+export default router;
