f943700b26fb543f0cb2ba8377a4e054f9accdf0
shafdo <shalinda@lesstaxi.com>
2024-05-12 01:11:58 +0530

    Chore: edit course details

--- frontend/src/pages/instructor/course/edit/index.jsx
+++ frontend/src/pages/instructor/course/edit/index.jsx
@@ -1,6 +1,45 @@
-import { FileInput } from '@material-tailwind/react';
+import { useEffect, useState } from 'react';
+import {
+  editCourseAPIWrapper,
+  getCourseAPIWrapper,
+} from '../../../../api/course';
+import { notify } from '../../../../utils/notifier';
 
 const CourseEditPage = () => {
+  const courseId = 'b4dfa464-4657-426e-aa5e-8918ace4ea92';
+
+  const [courseName, setCourseName] = useState('');
+  const [courseDesc, setCourseDesc] = useState('');
+  const [coursePrice, setCoursePrice] = useState('');
+
+  const fetchCourse = async (courseId) => {
+    const res = await getCourseAPIWrapper(courseId);
+    setCourseName(res.data.crsname);
+    setCourseDesc(res.data.description);
+    setCoursePrice(res.data.price);
+  };
+
+  const formHandler = async () => {
+    const payload = {
+      crsname: courseName,
+      description: courseDesc,
+      price: coursePrice,
+      courseId: courseId,
+    };
+
+    const res = await editCourseAPIWrapper(payload);
+    if (res.data.status) {
+      notify('Course updated successfully', 'success');
+      return;
+    }
+
+    notify('Course update failed', 'error');
+  };
+
+  useEffect(() => {
+    fetchCourse(courseId);
+  }, []);
+
   return (
     <>
       <p className="text-4xl text-center my-8">Edit Course</p>
@@ -15,8 +54,10 @@ const CourseEditPage = () => {
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="crsname"
+            value={courseName}
             type="text"
             placeholder="Course Name"
+            onChange={(e) => setCourseName(e.target.value)}
           />
         </div>
         <div className="mb-4">
@@ -27,9 +68,11 @@ const CourseEditPage = () => {
             Course Description
           </label>
           <textarea
+            value={courseDesc}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="description"
             placeholder="Course Description"
+            onChange={(e) => setCourseDesc(e.target.value)}
             rows={8}
           ></textarea>
         </div>
@@ -43,31 +86,19 @@ const CourseEditPage = () => {
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="price"
+            value={coursePrice}
             type="number"
             placeholder="Course Price"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="video"
-          >
-            Upload Video
-          </label>
-          <FileInput
-            color="blue"
-            size="sm"
-            outline={true}
-            id="video"
-            placeholder="Select a video file"
+            onChange={(e) => setCoursePrice(e.target.value)}
           />
         </div>
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
+            onClick={formHandler}
           >
-            Submit for reviewal
+            Edit Course
           </button>
         </div>
       </form>
