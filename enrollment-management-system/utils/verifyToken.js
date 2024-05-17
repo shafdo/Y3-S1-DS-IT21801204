1bf82694c197156fa6f9d120b911b6f656ac21dc
shafdo <shalinda@lesstaxi.com>
2024-05-17 07:38:23 +0530

    Frat: Initial Docker config added

--- enrollment-management-system/utils/verifyToken.js
+++ enrollment-management-system/utils/verifyToken.js
@@ -1,32 +1,28 @@
-import jwt from "jsonwebtoken";
-import { createError } from "./error.js";
-
-verifytoken.js
+import jwt from 'jsonwebtoken';
+import { createError } from './error.js';
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.auth;
+  const token = req.cookies.auth;
 
-    if(!token){
-        return next(createError(401, "You are not authenticated!"));
-    }
+  if (!token) {
+    return next(createError(401, 'You are not authenticated!'));
+  }
 
-    jwt.verify(token, process.env.JWT, (err, user) => {
-        if(err)
-            return next(createError(403, "Token is not valid!"));
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, 'Token is not valid!'));
 
-        req.user = user;
-        next();
-    
-    });2
-
+    req.user = user;
+    next();
+  });
+  2;
 };
 
 export const verifyUser = (req, res, next) => {
-    verifyToken(req, res, next, () => {
-        if(req.user.id === req.params.id){
-            next();
-        } else{
-             return next( createError(403, "You are not authorized!!!"));
-        }
-    })
-}
+  verifyToken(req, res, next, () => {
+    if (req.user.id === req.params.id) {
+      next();
+    } else {
+      return next(createError(403, 'You are not authorized!!!'));
+    }
+  });
+};
